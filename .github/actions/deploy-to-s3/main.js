f6eaeb5f2585eefa74cbfc2a3ad988d268b30687
Manjunatha Sai Uppu <manjunatha16.512@gmail.com>
2023-12-29 11:28:40 +0530

    Added Code for Uploading the data to the bucket

--- .github/actions/deploy-to-s3/main.js
+++ .github/actions/deploy-to-s3/main.js
@@ -2,7 +2,20 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 function run() {
-  core.notice('Hello from my JavaScript Custom Action');
+  const bucketName = core.getInput('bucket-name', { required: true });
+  const bucketRegion = core.getInput('bucketRegion', { required: true });
+  const destinationFolder = core.getInput('destination-folder', {
+    required: true,
+  });
+  core.notice(
+    `These are the Bucket Details Bucket Name is ${bucketName} in the region ${bucketRegion} and the destination folder would be ${destinationFolder} }`
+  );
+
+  //   Uplaod Files
+  //   const s3Uri = `s3://${bucketName}`;
+  //   exec.exec(
+  //     `aws s3 sync ${destinationFolder} ${s3Uri} --region ${bucketRegion}`
+  //   );
 }
 
 run();
